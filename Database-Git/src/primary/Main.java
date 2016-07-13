a997715d9f8161e4d2e1ceecbf8ccf0237d0ce70
specialk731 <kevingreenwald13@gmail.com>
2016-07-13 10:46:13 -0500

    Initialization 2

---
+++ Database-Git/src/primary/Main.java
@@ -0,0 +1,24 @@
+package primary;
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try{
+		PrintWriter writer = new PrintWriter("data/Test.txt", "UTF-8");
+		writer.println("The first line");
+		writer.println("The second line");
+		writer.close();
+		
+		System.out.println("Finished");
+	
+		}
+		catch (Exception e)
+		{
+			System.out.println("Exception");
+		}
+	}
+
+}
---
+++ Database-Git/src/primary/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author kevingreenwald
+ *
+ */
+package primary;
