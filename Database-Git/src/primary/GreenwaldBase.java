bce4e7ffd41d4e78277743bb6879b1409d8c2cb1
specialk731 <kevingreenwald13@gmail.com>
2016-07-14 19:49:01 -0500

    .tbl and .ndx added

--- Database-Git/src/primary/GreenwaldBase.java
+++ Database-Git/src/primary/GreenwaldBase.java
@@ -7,10 +7,15 @@ public class GreenwaldBase {
 	public static void main(String[] args) {
 		
 		try{
-		PrintWriter writer = new PrintWriter("data/Test.txt", "UTF-8");
+		PrintWriter writer = new PrintWriter("data/Table_1.tbl", "UTF-8");
 		writer.println("The first line");
 		writer.println("The second line");
 		writer.close();
+		
+		PrintWriter writer2 = new PrintWriter("data/Index_1.ndx", "UTF-8");
+		writer2.println("The first line");
+		writer2.println("The second line");
+		writer2.close();
 		
 		System.out.println("Finished");
 	
